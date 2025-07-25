6c5638b60f691ab4fe5e6584109c0bdd1ba47671
Rs2chel <rachelmeijom@gmail.com>
2025-07-25 20:17:08 -0300

    att menu bar

--- ExerciciosObrigatorios03/exercicio01/scripts.js
+++ ExerciciosObrigatorios03/exercicio01/scripts.js
@@ -1,35 +1,32 @@
-let menu = document.getElementById('menu')
-const topBtn = document.getElementById("topBtn");
-const navbar = document.getElementById("navbar");
-let navBlock = document.getElementsByClassName("links-menu")
+// let menu = document.getElementById('menu')
+// const topBtn = document.getElementById("topBtn");
+// const navbar = document.getElementById("navbar");
+// const navBlock = document.getElementById("links-menu")
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    menu.addEventListener("click", () => {
-        if (navBlock.style.display === "flex") {
-            navBlock.style.display = "none";
-        } else {
-            navBlock.style.display = "flex";
-            navBlock.style.flexDirection = "column";
-        }
+// document.addEventListener("DOMContentLoaded", () => {
 
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
 
-    });
-
-
-    window.addEventListener("scroll", () => {
-        const navBottom = navbar.getBoundingClientRect().bottom;
-
-        if (navBottom < 0) {
-            topBtn.style.display = "block";
-        } else {
-            topBtn.style.display = "none";
-        }
-    });
-
-    topBtn.addEventListener("click", () => {
-
-        console.log('clicado');
-    });
-
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
 });
+
+
+//     window.addEventListener("scroll", () => {
+//         const navBottom = navbar.getBoundingClientRect().bottom;
+
+//         if (navBottom < 0) {
+//             topBtn.style.display = "block";
+//         } else {
+//             topBtn.style.display = "none";
+//         }
+//     });
+
+//     topBtn.addEventListener("click", () => {
+
+//         console.log('clicado');
+//     });
+
+// });
